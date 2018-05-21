4d61280ddd461da46bcf68001af0bbe4e76af113
s-walrus <swalrus@yandex.ru>
2018-05-21 19:06:59 +0300

    Made a mockup

---
+++ main.js
@@ -0,0 +1,7 @@
+$(document).ready(function() {
+  $('#terminal').terminal(function(command, term) {
+
+  }, {
+    greetings: "JQuery Terminal:\nCopyright (c) 2011-2018 Jakub Jankiewicz <http://jcubic.pl/me>"
+  });
+});
