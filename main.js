8388ffac14cdc1117b24c51857b1fd001be26d0d
s-walrus <swalrus@yandex.ru>
2018-05-22 18:30:02 +0300

    Added an editor (CodeMirror)

--- main.js
+++ main.js
@@ -1,7 +1,27 @@
+CodeMirror.defineMode("thue", function() {
+  return {
+        token: function(stream, state) {
+
+            if (stream.match("->") ) {
+                return "keyword";
+            } else {
+                stream.next();
+                return "def";
+            }
+        }
+    };
+});
+
 $(document).ready(function() {
   $('#terminal').terminal(function(command, term) {
 
   }, {
     greetings: "JQuery Terminal:\nCopyright (c) 2011-2018 Jakub Jankiewicz <http://jcubic.pl/me>"
+  });
+
+  var codemirror = CodeMirror.fromTextArea($("textarea").get(0), {
+    theme: "duotone-dark",
+    lineNumbers: true,
+    mode: "thue"
   });
 });
