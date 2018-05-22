12e9acacd4997062fb1e746ea496d92619686423
s-walrus <swalrus@yandex.ru>
2018-05-22 20:24:05 +0300

    Added var terminal

--- main.js
+++ main.js
@@ -1,27 +1,29 @@
-CodeMirror.defineMode("thue", function() {
-  return {
-        token: function(stream, state) {
+var terminal;
 
-            if (stream.match("->") ) {
-                return "keyword";
-            } else {
-                stream.next();
-                return "def";
-            }
-        }
-    };
+CodeMirror.defineMode("thue", function() {
+	return {
+		token: function(stream, state) {
+
+			if (stream.match("->") ) {
+				return "keyword";
+			} else {
+				stream.next();
+				return "def";
+			}
+		}
+	};
 });
 
 $(document).ready(function() {
-  $('#terminal').terminal(function(command, term) {
+	terminal = $('#terminal').terminal(function(command, term) {
 
-  }, {
-    greetings: "JQuery Terminal:\nCopyright (c) 2011-2018 Jakub Jankiewicz <http://jcubic.pl/me>"
-  });
+	}, {
+		greetings: "JQuery Terminal:\nCopyright (c) 2011-2018 Jakub Jankiewicz <http://jcubic.pl/me>"
+	});
 
-  var codemirror = CodeMirror.fromTextArea($("textarea").get(0), {
-    theme: "duotone-dark",
-    lineNumbers: true,
-    mode: "thue"
-  });
+	var codemirror = CodeMirror.fromTextArea($("textarea").get(0), {
+		theme: "duotone-dark",
+		lineNumbers: true,
+		mode: "thue"
+	});
 });
