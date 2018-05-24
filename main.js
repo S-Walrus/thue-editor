c171852626858f8a912c0b0b5bb71907e90f97a5
s-walrus <swalrus@yandex.ru>
2018-05-24 15:59:48 +0300

    Added levels box to the page

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ var mainstring;
 var codemirror;
 var timerId;
 var started = false;
+var selectedLevel;
 
 CodeMirror.defineMode("thue", function() {
 	return {
@@ -19,26 +20,38 @@ CodeMirror.defineMode("thue", function() {
 });
 
 $(document).ready(function() {
+	// Init the terminal
 	terminal = $('#terminal').terminal(function(command, term) {
 
 	}, {
 		greetings: "JQuery Terminal:\nCopyright (c) 2011-2018 Jakub Jankiewicz <http://jcubic.pl/me>"
 	});
 
+// Init the editor
 	codemirror = CodeMirror.fromTextArea($("textarea").get(0), {
 		theme: "duotone-dark",
 		lineNumbers: true,
 		mode: "thue"
 	});
 
+	// Levels
+	$('.level').on('click', function() {
+		if (selectedLevel != null) {
+			selectedLevel.css('border-left', '0');
+		}
+		selectedLevel = $(this);
+		$(this).css('border-left', '6px solid #D7443F');
+	});
+
+	// Buttons
 	$('#run').on('click', function() {
 		run();
 	});
-	
+
 	$('#pause').on('click', function() {
 		pause();
 	});
-	
+
 	$('#stop').on('click', function() {
 		stop();
 	});
@@ -50,6 +63,7 @@ function run() {
 		mainstring = codemirror.getLine(codemirror.lastLine());
 	}
 	timerId = setInterval (function() {
+		var edited = false;
 		codemirror.eachLine(codemirror.firstLine(), codemirror.lastLine(), function(line) {
 			if (line.text == "") {
 				return 0;
@@ -62,9 +76,13 @@ function run() {
 					var b = mainstring.substring(n + line[0].length);
 					mainstring = a + line[1] + b;
 					terminal.set_prompt('[[b;green;]' + mainstring + ']');
+					edited = true;
 				}
 			}
-		})
+		});
+		if (!edited) {
+			stop();
+		}
 	}, 100);
 }
 
